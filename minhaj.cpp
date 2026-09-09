162b48e59004cfa0108e2c4dc5050f19fe9aac06
Md. Minhaj Uddin <c253055@ugrad.iiuc.ac.bd>
2026-09-09 22:46:43 +0600

    Update html

--- minhaj.cpp
+++ minhaj.cpp
@@ -1,1 +1,30 @@
--
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    
+    vector<int> a(n);
+    
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    
+    for (int i = 0; i < k; i++)
+    {
+        int x;
+        cin >> x;
+        
+
+        int pos = upper_bound(a.begin(), a.end(), x) - a.begin()+1;
+        
+        cout << pos << '\n'; 
+    }
+    
+    return 0;
+}
